6db94ccd769a674945848c7d1f8c1f7cfdcbc447
spandanakoppaku <skoppaku@gmail.com>
2023-08-07 16:50:33 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "docdb" {
 source                 = "./vendor/modules/docdb" 
 ENV                    = var.ENV
+DOCDB_INSTANCE_TYPE    = var.DOCDB_INSTANCE_TYPE
+DOCDB_INSTANCE_COUNT   = var.DOCDB_INSTANCE_COUNT
+DOCDB_PORT_NUMBER      = var.DOCDB_PORT_NUMBER
+
 }
 
 module "redis" {
--- vars.tf
+++ vars.tf
@@ -2,8 +2,14 @@
 variable "ENV" {}
 
 # RDS Variables
-variable MYSQL_PORT_NUMBER {}
-variable MYSQL_STORAGE {}
-variable MYSQL_ENGINE_VERSION {}
-variable MYSQL_INSTANCE_TYPE {}
+variable "MYSQL_PORT_NUMBER" {}
+variable "MYSQL_STORAGE" {}
+variable "MYSQL_ENGINE_VERSION" {}
+variable "MYSQL_INSTANCE_TYPE" {}
+
+# docdb Variables
+variable "DOCDB_INSTANCE_TYPE" {}
+variable "DOCDB_INSTANCE_COUNT" {}
+variable "DOCDB_PORT_NUMBER" {}
+
   
